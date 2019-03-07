5335c3e8de8630602a323e72bc6f97eb3b7eb699
joe <ebrightster@gmail.com>
2019-03-07 04:32:31 -0800

    Adding ability to validate all fields at once.

--- lib/src/validators/notEqualValidator.dart
+++ lib/src/validators/notEqualValidator.dart
@@ -11,7 +11,7 @@ class NotEqualValidator extends BaseValidator {
     _compareTo = compareTo;
     _compareToField = compareToField;
     if(_compareToField != null) {
-      message = "$key must equal $_compareToField";
+      message = "$key must not equal $_compareToField.";
     }
   }
 
