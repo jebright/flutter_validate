f61a14f9e39c803cf284d3cebb15a7e5139602bf
Ric17101 <ric17101raguine@gmail.com>
2021-10-08 13:44:36 +0800

    fix: test validators
    - fix validator test cases

--- test/contact.dart
+++ test/contact.dart
@@ -1,15 +1,15 @@
 import 'package:flutter_validate/flutter_validate.dart';
 
 class Contact {
-  String name;
-  DateTime dob;
+  String? name;
+  DateTime? dob;
   String contactPreference = "EMAIL"; //PHONE, EMAIL
-  String phone;
-  String email;
-  String password;
+  String? phone;
+  String? email;
+  String? password;
 }
 
 class ContactValidator extends AbstractValidator<Contact> {
   Contact contact;
-  ContactValidator(this.contact): super(contact);
+  ContactValidator(this.contact) : super(contact);
 }
--- test/lengthValidator_test.dart
+++ test/lengthValidator_test.dart
@@ -4,22 +4,21 @@ import 'package:flutter_validate/flutter_validate.dart';
 void main() {
   test('when string a is in range, length called, is true', () {
     String test = '1234567890';
-    expect(LengthValidator.length(test, 5,10), true);
+    expect(LengthValidator.length(test, 5, 10), true);
   });
 
   test('when string a is under min range, length called, is false', () {
     String test = '123';
-    expect(LengthValidator.length(test, 5,10), false);
+    expect(LengthValidator.length(test, 5, 10), false);
   });
 
   test('when string a is over max range, length called, is false', () {
     String test = '123456789012345';
-    expect(LengthValidator.length(test, 5,10), false);
+    expect(LengthValidator.length(test, 5, 10), false);
   });
 
-  test('when string is null, length called, does not throw', ()  {
-    String test;
-    expect(LengthValidator.length(test, 5,10), false);
+  test('when string is null, length called, does not throw', () {
+    String? test;
+    expect(LengthValidator.length(test, 5, 10), false);
   });
-
 }
